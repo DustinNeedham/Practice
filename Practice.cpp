5f1b3325d1d8eaf0de7bdf2b0271265077d1f59d
dneedham1 <dneedham1@atu.edu>
2024-01-23 13:59:42 -0600

    Add project files.

---
+++ Practice.cpp
@@ -0,0 +1,11 @@
+//Dustin Needham
+//January 23, 2024
+//GitHub Practice
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!\n";
+	std::cout << "My name is Dustin Needham!\n";
+	std::cout << "This is a GitHub upload practice!\n";
+}
